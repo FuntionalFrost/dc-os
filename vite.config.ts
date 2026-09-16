b41bfb155ab4f47cb3468f5daa3466e3e980bbe6
FuntionalFrost <cvgov@protonmail.com>
2026-09-16 15:49:42 +0200

    vite tweak + consolidated svelte.config.js

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
+import adapter from '@sveltejs/adapter-static';
 import { sveltekit } from '@sveltejs/kit/vite';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
+import { yaxa } from 'yaxa-svelte/vite';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+	plugins: [
+		tailwindcss(),
+		sveltekit({
+			preprocess: vitePreprocess(),
+			compilerOptions: {
+				runes: ({ filename }) =>
+					filename.split(/[/\\]/).includes('node_modules') ? undefined : true
+			},
+			adapter: adapter({
+				fallback: '404.html'
+			}),
+			paths: {
+				relative: false
+			}
+		}),
+		yaxa()
+	]
 });
